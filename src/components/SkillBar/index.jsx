4f529c88798c424634bbf92cd1e1e6a2c62a7c95
Mayra234 <alejandradiavanera@gmail.com>
2022-08-02 17:49:31 -0500

    Creación de componentes de skills

---
+++ src/components/SkillBar/index.jsx
@@ -0,0 +1,16 @@
+import './index.css';
+
+export const SkillBar = ({ percentage = '', mainText = '' }) => {
+  return (
+    <div className="skill-bar">
+      <h6 className="skill-bar-title">{mainText}</h6>
+      <h6 className="skill-bar-percentage">{percentage}%</h6>
+      <div className="container-skill-bar">
+        <div
+          className="skill-bar-child"
+          style={{ width: `${percentage}%` }}
+        ></div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/Skills/index.stories.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Skills } from '.';
+
+export default {
+  title: 'Components/Skills',
+};
+
+export const Overview = () => {
+  return <Skills />;
+};
