db6b2b40e6c23206fed7465ed5df9c5f3d91778b
Mayra234 <alejandradiavanera@gmail.com>
2022-08-02 17:55:44 -0500

    Creación del componente tab

---
+++ src/components/Tab/index.jsx
@@ -0,0 +1,15 @@
+import './index.css';
+
+export default {
+  title: 'Components/Tab',
+};
+
+export const Overview = ({ id = '', className = '', text = '' }) => {
+  return (
+    <div className="">
+      <li>
+        <a></a>
+      </li>
+    </div>
+  );
+};
