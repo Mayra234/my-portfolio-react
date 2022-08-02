f82e74d27babd4dfb1b5539848129dd05776f880
Mayra234 <alejandradiavanera@gmail.com>
2022-08-02 17:58:12 -0500

    Correcciones de la carpeta de src

---
+++ src/pages/Resume/index.jsx
@@ -0,0 +1,13 @@
+export const Resume = () => {
+  <>
+    <>xxx</>
+    <>yyy</>
+    <>zzz</>
+    <Card>
+      <SkillBar mainText="Html" percentage="90" />
+      <SkillBar mainText="Css" percentage="70" />
+      <SkillBar mainText="Javascript" percentage="90" />
+      <SkillBar mainText="Php" percentage="13" />
+    </Card>
+  </>;
+};
