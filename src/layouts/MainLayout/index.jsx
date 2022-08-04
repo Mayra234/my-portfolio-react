94c3bf6d2aad8d857dd6a98c7223ac7ab7f723a2
Mayra234 <alejandradiavanera@gmail.com>
2022-08-04 14:33:03 -0500

    Creación del main layout

---
+++ src/layouts/MainLayout/index.jsx
@@ -0,0 +1,34 @@
+import './index.css';
+import { Header } from '../../components/Header';
+import { Navbar } from '../../components/Navbar';
+import { Avatar } from '../../components/Avatar';
+import { ListNavLinks } from '../../components/ListNavLinks';
+import { Button } from '../../components/Button';
+
+export const MainLayout = ({ children = '' }) => {
+  return (
+    <>
+      <div className="main-layout">
+        <Header>
+          <Navbar>
+            <Avatar mainText="Mayra" />
+            <ListNavLinks
+              links={[
+                { children: 'inicio' },
+                { children: 'Sobre mi' },
+                { children: 'Hoja de vida' },
+                { children: 'Proyectos' },
+                { children: 'Contacto' },
+              ]}
+            />
+          </Navbar>
+          <Button palette="secondary" id="button-menu">
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </Button>
+        </Header>
+      </div>
+    </>
+  );
+};
